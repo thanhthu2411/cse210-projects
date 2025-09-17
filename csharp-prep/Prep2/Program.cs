d85f8b60b12d7c85a2eb62b370be156353fe14b3
thanhthu2411 <thu24@byui.edu>
2025-09-17 12:42:16 -0600

    w1 prove assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Your grade percentage: ");
+        string percentage = Console.ReadLine();
+
+        int per = int.Parse(percentage);
+        string letter = "";
+        string sign = "";
+
+        if (per >= 90)
+        {
+            letter = "A";
+        }
+        else if (per >= 80)
+        {
+            letter = "B";
+
+        }
+        else if (per >= 70)
+        {
+            letter = "C";
+
+        }
+        else if (per >= 60)
+        {
+            letter = "D";
+
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // sign for grades
+        if (93 > per && per >= 60)
+        {
+            if (per % 10 >= 7)
+            {
+                sign = "+";
+            }
+            else if (per % 10 < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        Console.WriteLine($"{letter}{sign}");
+
+        // pass or fail class
+        if (per >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Try harder!");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.Write("What?");
+        string color = Console.ReadLine();
     }
 }
