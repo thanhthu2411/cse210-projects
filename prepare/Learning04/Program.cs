aac1a56b7d5e0a0f0a4452c08dc27da1d17a2ff0
thanhthu2411 <thu24@byui.edu>
2025-10-25 19:29:09 -0600

    Learning04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment math1 = new MathAssignment("Anna", "Fraction", "7.3", "8-19");
+        WritingAssignment writing1 = new WritingAssignment("Harry", "Counter-argument", "Happiness");
+
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+        Console.WriteLine(writing1.GetSummary());
+        Console.WriteLine(writing1.GetWritingInformation());
+
     }
 }
